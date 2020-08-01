10803db168ac541671632691c9d2812acf0ce26f
rkildare <55807922+rkildare@users.noreply.github.com>
2020-07-31 17:17:48 -0700

    Fixed tab size in Ind_Xray.rb

--- Ind_Xray.rb
+++ Ind_Xray.rb
@@ -2,14 +2,14 @@ require 'sketchup.rb'
 require 'extensions.rb'
 
 module Rkildare
-    module IndXray
-        unless file_loaded?(__FILE__)
-            ex = SketchupExtension.new('Individual Xray', 'Ind_Xray/main')
-            ex.description = 'Xray view creater for individual groups.'
-            ex.version = '0.3a'
-            ex.creator = 'rkildare'
-            Sketchup.register_extension(ex,true)
-            file_loaded(__FILE__)
-        end
+  module IndXray
+    unless file_loaded?(__FILE__)
+      ex = SketchupExtension.new('Individual Xray', 'Ind_Xray/main')
+      ex.description = 'Xray view creater for individual groups.'
+      ex.version = '0.3a'
+      ex.creator = 'rkildare'
+      Sketchup.register_extension(ex,true)
+      file_loaded(__FILE__)
     end
+  end
 end
