da9d200abc79b331e350e5155efe1202b37ebcbc
rkildare <55807922+rkildare@users.noreply.github.com>
2020-09-23 09:32:13 -0700

    Update Ind_Xray.rb

--- Ind_Xray.rb
+++ Ind_Xray.rb
@@ -5,7 +5,7 @@ module Rkildare
     unless file_loaded?(__FILE__)
       ex = SketchupExtension.new('Individual Xray', 'Ind_Xray/main')
       ex.description = 'Xray view creater for individual groups.'
-      ex.version = '1.0a'
+      ex.version = '1.0b'
       ex.creator = 'rkildare'
       Sketchup.register_extension(ex,true)
       file_loaded(__FILE__)
